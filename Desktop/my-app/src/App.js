15290c30f1aeaf6431b0b3179dfe0b8a821834e8
Joe Naples <jnaples90@gmail.com>
2021-10-17 18:14:58 -0400

    Nav links

---
+++ Desktop/my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import NavLinks from "./components/UI/NavLinks";
+
+const App = () => {
+  return (
+    <div className='App'>
+      <NavLinks />
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ Desktop/my-app/src/components/UI/NavLinks.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./NavLinks.module.css";
+
+const NavLinks = () => {
+  return (
+    <div>
+      <ul className='hello'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavLinks;
